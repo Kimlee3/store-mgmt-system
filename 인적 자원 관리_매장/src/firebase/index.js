dad92bfee20461a8d33d6e661f72d727f5677f9b
Kimlee3 <kimleekwan3@gmail.com>
2025-05-25 16:09:59 +0200

    ✅ fix: Firebase GitHub Actions 워크플로우 최종 수정

--- 인적 자원 관리_매장/src/firebase/index.js
+++ 인적 자원 관리_매장/src/firebase/index.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "복붙",
-  authDomain: "복붙.firebaseapp.com",
-  projectId: "복붙",
-  storageBucket: "복붙.appspot.com",
-  messagingSenderId: "복붙",
-  appId: "복붙"
+  apiKey: "AIzaSyB0X5LMzxSP13YSpoomU57o3xQjkvdRvXI",
+  authDomain: "store-mgmt-system.firebaseapp.com",
+  projectId: "store-mgmt-system",
+  storageBucket: "store-mgmt-system.appspot.com",
+  messagingSenderId: "1003262628346",
+  appId: "1:1003262628346:web:4fbd566de41d6c1c7f9443"
 };
 
 const app = initializeApp(firebaseConfig);
